9cbb583d63a9e36d3ca2fd1b972a5308a19252c5
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2024-07-11 16:01:50 +0700

    chore : setup tailwind and folder alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import '@styles/tailwind.scss';
+import Home from './pages/Home.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <Home />
+  </React.StrictMode>
+);
