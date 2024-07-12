b74c8a70309c008696dea09edbe031417e691405
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2024-07-12 07:41:43 +0700

    chore : add helpers moment

---
+++ src/lib/helpers.ts
@@ -0,0 +1,14 @@
+import moment from 'moment';
+
+export const convertDateMoment = (
+  dateString: string,
+  format: string = 'YYYY-MM-DD'
+) => moment(dateString).format(format);
+
+export const createSlug = (text: string) =>
+  text
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-\s]/g, '-')
+    .replace(/\s+/g, '-')
+    .replace(/--+/g, '-')
+    .replace(/^-|-$/g, '');
